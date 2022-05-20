e48397e5dd81053219aee1d161039ef74d7494a2
NGennrich <nicolai.gennrich@emmy-sharing.de>
2022-05-20 13:38:30 +0200

    change get_ready to be a parameter

--- wakeapp/controller/time_controller.py
+++ wakeapp/controller/time_controller.py
@@ -2,17 +2,15 @@ from datetime import datetime, time
 
 
 class TimeController:
-    def __init__(self, arrival:datetime=None, depature:datetime=None, get_ready:time=None) -> None:
+    def __init__(self, arrival:datetime=None, depature:datetime=None) -> None:
         if arrival is None:
             self.arrival = datetime.now()
         if depature is None:
             self.depature = datetime.now()
-        if get_ready is None:
-            self.get_ready = time(hour=0,minute=30,second=0)
 
     def calculate_travel_time(self) -> time:
         ...
 
     
-    def get_wakeup_datetime(self) -> datetime:
+    def get_wakeup_datetime(self, get_ready: time) -> datetime:
         ...
